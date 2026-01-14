a85ec4d52f4a0a2d8129986688d4409fe917ff3b
SathiyamoorthiMurugesan <sathya08ece@gmail.com>
2026-01-14 23:22:11 +0530

    Casting,  scrolling and JS

--- src/main/java/e5WebDriverMethods/B7ScrollingInSelenium.java
+++ src/main/java/e5WebDriverMethods/B7ScrollingInSelenium.java
@@ -15,39 +15,40 @@ public class B7ScrollingInSelenium {
 	static WebDriver driver;
 
 	public static void main(String[] args) throws Exception {
-		System.setProperty("webdriver.chrome.driver", ".\\src\\main\\resources\\drivers\\chromedriver_136.exe");
+//		System.setProperty("webdriver.chrome.driver", ".\\src\\main\\resources\\drivers\\chromedriver_136.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.geeksforgeeks.org/javascriptexecutor-in-selenium/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		
 
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 
-//		 js.executeScript("window.scrollBy(0,150)");
-//		Thread.sleep(2000);
+		js.executeScript("window.scrollBy(0,150)");
+		Thread.sleep(2000);
 
 //		To go to bottom of the page
-//		js.executeScript("window.scrollBy(0,5000)");
+		js.executeScript("window.scrollBy(0,5000)");
 
-//		Thread.sleep(2000);
+		Thread.sleep(2000);
 
-		
-//		js.executeScript("window.scrollTo(0, 0)");
-		 
-//		 js.executeScript("alert('Welcome to Selenium');");
+		js.executeScript("window.scrollTo(0, 0)");
+		Thread.sleep(2000);
+
+//		 js.executeScript("alert('Welcome to Selenium123');");
 //
 //		WebElement element = driver.findElement(By.className("footer_advertise_btn"));
 //		 js.executeScript("arguments[0].scrollIntoView();", element); 
 //
-		 WebElement ele = driver.findElement(By.className("facebook"));
-//		 js.executeScript("arguments[0].scrollIntoView();", ele); 
-//		 Thread.sleep(2000);
+		WebElement ele = driver.findElement(By.className("facebook"));
+		js.executeScript("arguments[0].scrollIntoView();", ele);
+		Thread.sleep(2000);
 //		 ele.click();
 		 js.executeScript("arguments[0].click();", ele);
-//
-////		 js.executeScript("arguments[0].value='---your email id---';", element);
+		 Thread.sleep(2000);
 		 
+//		 WebElement element = driver.findElement(By.name("email"));
+//		 js.executeScript("arguments[0].value='sathiyamoorthi@gmail.com';", element);
+
 	}
 
 	public void scrollToBottomOfThePage() {
@@ -59,21 +60,20 @@ public class B7ScrollingInSelenium {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,0)");
 	}
-	
+
 	public void scrollToTheGivenElementUsingJS(WebElement element) {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-		 js.executeScript("arguments[0].scrollIntoView();", element); 
+		js.executeScript("arguments[0].scrollIntoView();", element);
 	}
-	
 
 	public void clickTheGivenElementUsingJS(WebElement element) {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].click();", element);
 	}
-	
+
 	public void sendTextToAnElement(WebElement ele) {
-		JavascriptExecutor jse = (JavascriptExecutor)driver;
-		 jse.executeScript("arguments[0].value='enter the value here';", ele);
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		jse.executeScript("arguments[0].value='enter the value here';", ele);
 	}
-	
+
 }
